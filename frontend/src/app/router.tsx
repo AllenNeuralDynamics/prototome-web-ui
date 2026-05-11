3d44f3bea59c8b5e2f1b14e9713947468be93960
jessy.liao <jessy.liao@alleninstitute.org>
2026-05-11 12:05:34 -0700

    Remove configs passed into routes

--- frontend/src/app/router.tsx
+++ frontend/src/app/router.tsx
@@ -1,18 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { HomePage } from "./routes/HomePage.tsx";
 import { StagesPage } from "./routes/StagesPage.tsx";
-import type { AppConfig } from "@/types/configTypes.tsx";
 import { PylassoPage } from "./routes/PylassoPage.tsx";
 
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route
-        path="/"
-        element={<HomePage config={config} setConfig={setConfig} />}
-      />
-      <Route path="/stage" element={<StagesPage config={config} />} />
-      <Route path="/pylasso" element={<PylassoPage config={config} />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/stage" element={<StagesPage />} />
+      <Route path="/pylasso" element={<PylassoPage />} />
     </Routes>
   );
 };
--- frontend/src/app/routes/PylassoPage.tsx
+++ frontend/src/app/routes/PylassoPage.tsx
@@ -4,17 +4,16 @@ import {
   WaferCalibrationControl,
 } from "@/features/pylasso/index";
 
-import type { AppConfig } from "@/types/configTypes.tsx";
 import { Container, Stack } from "@mantine/core";
 
-export const PylassoPage = ({ config }: { config: AppConfig }) => {
+export const PylassoPage = () => {
   return (
     <Container>
       <Stack>
         <LassoCamera cameraId="window2_web_camera" />
         <LassoControl />
         <WaferCalibrationControl />
-      </Stack> 
+      </Stack>
     </Container>
   );
 };
